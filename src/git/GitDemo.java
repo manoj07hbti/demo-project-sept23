bd517ef6b1171fecb79a9d379bd648a9dc8bd2f5
Manoj Kumar <manojkumar.pcf@gmail.com>
2023-10-05 21:01:43 +0530

    this is for git commit demo2

--- src/git/GitDemo.java
+++ src/git/GitDemo.java
@@ -4,6 +4,10 @@ public class GitDemo {
 
     public static void main(String[] args) {
 
+
+
+        System.out.println("This is for git demo......");
+        System.out.println("This is for git demo......");
         System.out.println("This is for git demo......");
     }
 }
