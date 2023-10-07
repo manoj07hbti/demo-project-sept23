a47da35caaf8413fb791e60be2b349496b4f34dc
Mayank6918-svh <mayanksingh25104@gmail.com>
2023-10-07 21:11:04 +0530

    1ST COMMIT

---
+++ src/Javaclass.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class Javaclass {
+
+    public static void main(String args[]) {
+
+        System.out.println("Hello, This is my first Java class...");
+        System.out.println("THis is second line");
+        System.out.println("THis is Third line");
+
+    }
+}
