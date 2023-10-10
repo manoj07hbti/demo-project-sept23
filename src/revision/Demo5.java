899dfb85a3a51f2868229728dfc871268f13dbae
sachin31java <Sachin99kumar59@gmail.com>
2023-10-10 23:23:08 +0530

    Assignment of class and packages

---
+++ src/revision/Demo5.java
@@ -0,0 +1,13 @@
+package revision;
+
+public class Demo5 {
+    public static void main(String[]args) {
+        System.out.println("This is about my java teacher");
+        System.out.println("Manoj sir is nice and adorable guy ");
+        System.out.println("He is tall and muscular ");
+        System.out.println("He has excellent knowledge of java");
+        System.out.println("He is impressive person");
+
+
+    }
+}
