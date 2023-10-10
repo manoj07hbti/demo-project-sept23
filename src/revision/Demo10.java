89720bfd43772546d3b27e66ab45484cbf5537bc
sachin31java <Sachin99kumar59@gmail.com>
2023-10-10 23:52:08 +0530

    Assignment of class and packages

---
+++ src/revision/Demo10.java
@@ -0,0 +1,13 @@
+package revision;
+
+public class Demo10 {
+    public static void main(String[]args) {
+        System.out.println("Last project class");
+        System.out.println("Fourth line of this project ");
+        System.out.println("Third line of this project");
+        System.out.println("Second line of project");
+        System.out.println("Completed all class by me");
+
+
+    }
+}
