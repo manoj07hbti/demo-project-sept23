84eb8bc65fcd9ca7b6f994bc7cf1f9fce466100d
sachin31java <Sachin99kumar59@gmail.com>
2023-10-10 23:29:19 +0530

    Assignment of class and packages

---
+++ src/revision/Demo6.java
@@ -0,0 +1,11 @@
+package revision;
+
+public class Demo6 {
+    public static void main(String[]args){
+        System.out.println("Now i am drinking a cup of coffee");
+        System.out.println("Do not take seriously my first line ");
+        System.out.println("So i wrote third line of this class");
+        System.out.println("I wrote another line of class");
+        System.out.println("This is last ");
+    }
+}
