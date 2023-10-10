1a40640cf5710d6c5ccd8cd52dbfec633ad4090d
sachin31java <Sachin99kumar59@gmail.com>
2023-10-10 23:46:58 +0530

    Assignment of class and packages

---
+++ src/revision/Demo9.java
@@ -0,0 +1,13 @@
+package revision;
+
+public class Demo9 {
+    public static void main(String[]args) {
+        System.out.println("My name is sachin");
+        System.out.println("I am java learner");
+        System.out.println("I am beginner of this class");
+        System.out.println("I have been joining this class last september");
+        System.out.println("Only tuesday thursday of saturday i atted java class");
+
+
+}
+}
