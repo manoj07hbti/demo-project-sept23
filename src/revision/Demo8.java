e36f4955febbeb94062a759c4cac704d0ce7cfbb
sachin31java <Sachin99kumar59@gmail.com>
2023-10-10 23:40:34 +0530

    Assignment of class and packages

---
+++ src/revision/Demo8.java
@@ -0,0 +1,12 @@
+package revision;
+
+public class Demo8 {
+    public static void main(String[]args){
+       System.out.println("First line ");
+       System.out.println("Second line of this class");
+       System.out.println("Third line of this class");
+       System.out.println("Fourth line of my class ");
+       System.out.println("Last line of the eight class page");
+
+    }
+}
