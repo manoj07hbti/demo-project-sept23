2775c150ba8939dcd584d547d30a5b97243afc10
shub07hcst <shub6240@gmail.com>
2023-10-09 19:23:51 -0700

    This is second git commit demo

--- src/Context.java
+++ src/Context.java
@@ -1,3 +1,11 @@
-public class Context git{
+import java.net.SocketOption;
+
+public class Context {
+    public static void main(String[] args) {
+        String name="Dell laptop";
+        int price=42000;
+        String country="America";
+        System.out.println("Laptop name is: "+name +" Laptop price: "+price+" Country name: "+country);
+    }
 
 }
