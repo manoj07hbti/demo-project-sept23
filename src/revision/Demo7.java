0cfee6a53d758079087cd88029a111b87ed1d4f7
sachin31java <Sachin99kumar59@gmail.com>
2023-10-10 23:36:19 +0530

    Assignment of class and packages

---
+++ src/revision/Demo7.java
@@ -0,0 +1,13 @@
+package revision;
+
+public class Demo7 {
+    public static void main(String[] args) {
+        System.out.println("I have old laptop");
+        System.out.println("I think it should be called refurbished");
+        System.out.println("Laptop looks like a small box");
+        System.out.println("Laptop have many numerical and alpha keys");
+        System.out.println("My laptop colours black");
+
+
+    }
+}
