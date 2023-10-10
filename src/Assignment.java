0f9d73977301aff93fe9527d0bd7d3bad4313fb6
tannu-8 <tanubhushan08@gmail.com>
2023-10-10 13:09:33 +0530

    Variables Assignment

---
+++ src/Assignment.java
@@ -0,0 +1,47 @@
+public class Assignment {
+    public static void main(String[] args) {
+        //5 String variables
+
+
+        String greet = "Welcome";
+        System.out.println(greet);
+
+
+        String firstName = "Tannu";
+        System.out.println(firstName);
+
+
+        String lastname = "Bhushan";
+        System.out.println(lastname);
+
+
+        String schoolName = "ST Marys Sr. Sec. School";
+        System.out.println(schoolName);
+
+
+        String emailId = "Thisismy_mailId@1011";
+        System.out.println(emailId);
+
+
+        //5 int variables
+        int age = 20;
+        System.out.println(age);
+
+
+        int minValue = -9;
+        System.out.println(minValue);
+
+
+        int maxValue = 26527;
+        System.out.println(maxValue);
+
+
+        int evenNum = 824;
+        System.out.println(evenNum);
+
+
+        int oddNum = 625;
+        System.out.println(oddNum);
+
+    }
+}
