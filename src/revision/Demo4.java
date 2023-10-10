58a23d6b1377c0845e32e79eeb226547b8d3b4c7
sachin31java <Sachin99kumar59@gmail.com>
2023-10-10 23:16:23 +0530

    Assignment of class and packages

---
+++ src/revision/Demo4.java
@@ -0,0 +1,16 @@
+package revision;
+
+public class Demo4 {
+    public static void main(String[]args) {
+        System.out.println("Here we again for doing same work which was done in previous class");
+        System.out.println("Okay, i m good and sit on my chair last two hour");
+        System.out.println("Finally i learned some types related skills ");
+        System.out.println("Every student must learn type related skills");
+        System.out.println("Hey, you what are you doing now");
+        System.out.println("I am doing coding you know coding");
+
+
+
+    }
+
+}
