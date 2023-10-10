066f9d3d99efa7eab6bb613359e1c800de7be931
Manoj Kumar <manojkumar.pcf@gmail.com>
2023-10-10 21:28:50 +0530

    Variable declaration , assignment and printing

---
+++ src/demo_variables/Revision.java
@@ -0,0 +1,27 @@
+package demo_variables;
+
+public class Revision {
+
+    public static void main(String[] args) {
+        // SYNTAX : Datatype varName;
+
+        String name;  // we have declared a variable of String datatype with name as name
+
+        name="Mukesh"; // always use "" for String
+     // System.out.println("Message : " +varName);
+        System.out.println("Printing name "+name);
+
+        int age; // we have declared age variable of int datatype
+
+        age=23;
+        System.out.println("Printing age "+age);
+
+        double salary; // we have declared salary variable of double datatype.
+
+        salary=32432.45;
+        System.out.println("printing Salary "+salary);
+
+
+
+    }
+}
