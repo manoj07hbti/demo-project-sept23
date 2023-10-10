99a415ff623d5529957a3263b52de1747443802e
sachin31java <Sachin99kumar59@gmail.com>
2023-10-10 22:56:44 +0530

    Assignment of class and packages

---
+++ src/revision/Demo2.java
@@ -0,0 +1,14 @@
+package revision;
+
+public class Demo2 {
+    public static void main(String[]args) {
+        System.out.println("This is the second practise class of learning");
+        System.out.println("It provides some additional knowledge of language");
+        System.out.println("I learned some ahead knowledge of java");
+        System.out.println("Java project has many steps");
+        System.out.println("First steps of create project is tap on file then opt new and click");
+
+
+
+    }
+}
