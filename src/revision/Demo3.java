f6f539a2f986b8192c52bda5ad8d7c14272dcdd2
sachin31java <Sachin99kumar59@gmail.com>
2023-10-10 23:07:37 +0530

    Assignment of class and packages

---
+++ src/revision/Demo3.java
@@ -0,0 +1,14 @@
+package revision;
+
+public class Demo3 {
+    public static void main(String[]args) {
+        System.out.println("Hey, guys i am here again for learning java");
+        System.out.println("I am new learner of this new field like information technology");
+        System.out.println("Ongoing era is information technology era which provide quickly result ");
+        System.out.println("Information in data form much easier to collect and use or reuse in comparison with paper");
+        System.out.println("This is exhausted work when i write same line again and again but interesting");
+        System.out.println(" Now i click on this code");
+
+
+    }
+}
