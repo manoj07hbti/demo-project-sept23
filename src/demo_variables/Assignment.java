a2b52f5a83cf84d757f8c2957922797732208442
sachin31java <Sachin99kumar59@gmail.com>
2023-10-10 19:45:35 +0530

    This is demo commit second

--- src/demo_variables/Assignment.java
+++ src/demo_variables/Assignment.java
@@ -3,5 +3,6 @@ package demo_variables;
 public class Assignment {
     public static void main(String[]args){
         System.out.println("This is git checkin demo");
+        System.out.println("This is git checkin second line");
     }
 }
