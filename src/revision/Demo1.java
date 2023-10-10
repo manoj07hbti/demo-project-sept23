3a18f30b1bb31322f4e7503c0b02f9d70090faa3
sachin31java <Sachin99kumar59@gmail.com>
2023-10-10 22:47:32 +0530

    Assignment of class and packages

---
+++ src/revision/Demo1.java
@@ -0,0 +1,13 @@
+package revision;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is java learning class");
+        System.out.println("I am new learner of java language");
+        System.out.println("This language is useful in software development");
+        System.out.println("Java is mostly used in information technology");
+        System.out.println("Java looks like a difficult but not");
+        System.out.println("I am enjoying in this learning session");
+
+    }
+}
