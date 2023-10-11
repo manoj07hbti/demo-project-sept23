65291ddfbb2ea07cf38dbc31552987ebd1fcb0ae
Shree <rohitfzd9897@gmail.com>
2023-10-11 14:12:08 +0530

    revision_dEMO4

---
+++ src/revision/Demo_4.java
@@ -0,0 +1,35 @@
+package revision;
+
+public class Demo_4 {
+
+    public static void main(String[]args){
+
+
+
+        System.out.println("Do know you about the pollution ");
+
+        System.out.println("Kind of pollution ");
+
+        System.out.println("The pollution is biggest problem in India");
+
+        System.out.println(" Kind of pollution.... Air Pollation ; Water pollation ; Sound Pollution ");
+
+        System.out.println("AIr pollution  ....this is most dangers pollation one of them " +
+                "because, Air pollution main cause.... CO2,  ");
+
+        System.out.println("Water pollution main cause .....factory chemical water ");
+
+        System.out.println(" Sound main cause ..... sound speakers and bike ,car ,bus ,track,and other loudly" +
+                "sounds ");
+
+        System.out.println("These are some cause of pollution");
+
+    }
+
+
+
+
+
+
+
+}
