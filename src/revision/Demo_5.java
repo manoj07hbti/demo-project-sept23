2e6156f58b32505e75f70e8280018f515fd158e1
Shree <rohitfzd9897@gmail.com>
2023-10-11 14:32:29 +0530

    revision_dEMO4

---
+++ src/revision/Demo_5.java
@@ -0,0 +1,48 @@
+package revision;
+
+public class Demo_5 {
+
+public static void main(String[]args){
+
+    System.out.println( " Hello, My self Rohit ");
+
+     System.out.println(" I am 22 years old ");
+
+     System.out.println(" I had passed 10th in 2016 ");
+
+     System.out.println(" I have passed 12th in 2018 ");
+
+     System.out.println(" I had completed Graduation  in 2021  with  COMPUTER SCIENCE");
+
+
+
+     System.out.println(" My hobbies.....Playing cricket, Playing gaming , reading book ..... ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
