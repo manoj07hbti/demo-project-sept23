a386dbba9d4195b79943182df1e03d6ba737c955
sachin31java <Sachin99kumar59@gmail.com>
2023-10-11 12:39:55 +0530

    Assignment of variable declaration

---
+++ src/demo_variables/AssignmentVariable.java
@@ -0,0 +1,52 @@
+package demo_variables;
+
+public class AssignmentVariable {
+    public static void main(String[] args) {
+      // create 5 String variable
+        String name;
+        name="Sachin";
+        String CarBrandName;
+        CarBrandName="ferrari";
+        String LaptopName;
+        LaptopName="Lenovo";
+        String BookName;
+        BookName="Science Reporter";
+        String CityName;
+        CityName="kolkata";
+        String CityLocation;
+        CityLocation="West Bengal";
+        System.out.println("Printing name:"+ name);
+        System.out.println("Printing CarBrandName:"+ CarBrandName);
+        System.out.println("Printing LaptopName:"+LaptopName);
+        System.out.println("Printing BookName:"+BookName);
+        System.out.println("Printing CityName:"+CityName);
+        System.out.println("Printing CityLocation:"+CityLocation);
+
+        // create 5 int variable
+        int age;// I have declared age variable of int datatype
+        age=28;
+        int weight;// I have declared weight variable of int datatype
+        weight=80;
+        int population;// I have declared population variable of int datatype
+        population=80000;
+        double height;// I have declared height variable of double datatype
+
+        height=5.11;
+        double price;// I have declared price variable of double datatype
+        price=299.50;
+        double distance;// I have declared distance variable of double datatype
+        distance=10.5;
+
+        System.out.println("Printing age:"+age);
+        System.out.println("Printing weight:"+weight);
+        System.out.println("Printing population:"+population);
+        System.out.println("Printing height:"+height);
+        System.out.println("Printing price:"+price);
+        System.out.println("Printing distance:"+distance);
+
+
+
+
+
+    }
+}
