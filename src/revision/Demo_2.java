56b5d9b3bc1b4fac7fc4ee58014896d741fe118c
Shree <rohitfzd9897@gmail.com>
2023-10-11 12:28:15 +0530

    revision_demo_2&3

---
+++ src/revision/Demo_2.java
@@ -0,0 +1,44 @@
+package revision;
+
+public class Demo_2 {
+
+    public static void main(String []args ){
+
+        System.out.println("  I  am  INDIAN ");
+
+        System.out.println("  So' I am froud of  INDIAN ");
+
+        System.out.println("  Because; The INDIA is a most deplpment country in the world");
+
+        System.out.println("  The India is 3rd largest economy in the world ");
+
+        System.out.println("  Indian most growing fields  ..... IT sector; Medical Sector; and some others sector " );
+
+        System.out.println("  But; IT Sector is faster growing sector in the world ");
+
+        System.out.println(   "Some ; faster growing city in India......NEW DEHLI , PUNE; CHENNEI ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
