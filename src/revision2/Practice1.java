cbaefa200c61cdea7ca3eb83cfa08e2a5afaf4a9
shiv8383 <shiv83833@gmail.com>
2023-10-12 17:24:06 +0530

    java class

---
+++ src/revision2/Practice1.java
@@ -0,0 +1,15 @@
+package revision2;
+
+public class Practice1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" This is my first java class....");
+        System.out.println(" I have got car....");
+        System.out.println(" I have got bike....");
+        System.out.println(" I have got laptop....");
+        System.out.println(" I have got mobile....");
+
+
+    }
+}
