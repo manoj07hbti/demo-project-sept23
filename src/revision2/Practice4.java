e3d3ab4357d880d3558ba44e18f5a0bb9765a343
shiv8383 <shiv83833@gmail.com>
2023-10-12 18:27:20 +0530

    java class

---
+++ src/revision2/Practice4.java
@@ -0,0 +1,13 @@
+package revision2;
+
+public class Practice4 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" This is my fourth java class....");
+        System.out.println(" Start a business....");
+        System.out.println(" Put on jeans....");
+        System.out.println(" Speak on podium....");
+        System.out.println(" Hang around with friends....");
+    }
+}
