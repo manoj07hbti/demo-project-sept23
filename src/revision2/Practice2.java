b6c44ba9270ab027709ba57e4aec7a36b58e00b7
shiv8383 <shiv83833@gmail.com>
2023-10-12 18:26:24 +0530

    java class

---
+++ src/revision2/Practice2.java
@@ -0,0 +1,13 @@
+package revision2;
+
+public class Practice2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" This my second java class....");
+        System.out.println(" I can run....");
+        System.out.println(" He can run....");
+        System.out.println(" You can run....");
+        System.out.println(" She can run....");
+    }
+}
