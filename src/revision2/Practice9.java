a7440c9845bc363f22d6fd1c23cdce052d4072a1
shiv8383 <shiv83833@gmail.com>
2023-10-12 18:28:58 +0530

    java class

---
+++ src/revision2/Practice9.java
@@ -0,0 +1,13 @@
+package revision2;
+
+public class Practice9 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" This is my ninth java class");
+        System.out.println(" I went to delhi by flight....");
+        System.out.println(" I do the job for earning money....");
+        System.out.println(" I wanna purchase many cloths....");
+        System.out.println(" You should write....");
+    }
+}
