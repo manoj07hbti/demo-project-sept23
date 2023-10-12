c43166f51687d842b1ba74c2ce98f7efec12b1a5
Shree <rohitfzd9897@gmail.com>
2023-10-12 19:58:08 +0530

    revision_variable

---
+++ src/demo_variables/variable_revision/Test_2.java
@@ -0,0 +1,51 @@
+package demo_variables.variable_revision;
+
+public class Test_2 {
+
+     public static void main(String []args){
+
+         String cityName;
+
+         cityName="  Agra ";
+
+         System.out.println("cityName"+ cityName );
+
+         System.out.println( "  Agra is famous for TAJMAHAL ");
+
+         String locatedName;
+
+         locatedName= "UTTAR PRADESH";
+
+         int population;
+
+         population= 53645;
+
+         double income;
+
+         income=43543.45;
+
+
+
+     String visitingPlace;
+
+     visitingPlace= "TAJ MAHAL  RED FORD , ...........";
+
+         System .out.println("  population  "  +population  +"  income  "  +  income +
+
+                 " visitingPlace  "  +visitingPlace);
+
+
+
+
+
+     }
+
+
+
+
+
+
+
+
+
+}
