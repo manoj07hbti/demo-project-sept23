fb732504fed65fd7f1eb9035ab4cf1e3c92c2a4d
shiv8383 <shiv83833@gmail.com>
2023-10-12 18:28:19 +0530

    java class

---
+++ src/revision2/Practice7.java
@@ -0,0 +1,13 @@
+package revision2;
+
+public class Practice7 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" This is my seventh java class");
+        System.out.println(" I took  pizza on party....");
+        System.out.println(" I took tea....");
+        System.out.println(" I used to play cricket....");
+        System.out.println(" I used to play football....");
+    }
+}
