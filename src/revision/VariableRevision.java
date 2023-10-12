df3ab66d231ccda09a0b9172373670b7dd301542
Vishal Kumar <vk9555411@gmail.com>
2023-10-12 19:46:27 +0530

    Revision of Variables

---
+++ src/revision/VariableRevision.java
@@ -0,0 +1,67 @@
+package revision;
+
+import javax.sound.midi.Soundbank;
+
+public class VariableRevision {
+
+    public static void main(String[] args) {
+
+  // // SYNTAX : Datatype varName;
+
+        String OceanName;
+        OceanName="Pacific Ocean";
+        System.out.println("Print name of ocean: "+OceanName);
+
+        String MobileName="Samsung";
+        System.out.println("Printing the mobile Name: "+MobileName);
+
+        String PlanetName="Earth";
+        System.out.println("Print name of Planet: "+PlanetName);
+
+        String LaptopName="Lenovo";
+        System.out.println("Print laptop Name: "+LaptopName);
+
+        String FriendName="Deepanshu";
+        System.out.println("Print name of best friend: "+FriendName);
+
+        // SYNTAX : Datatype varName;
+
+        int age = 26;
+        System.out.println("Printing age of Deepanshu: "+age);
+
+        int Population = 98758875;
+        System.out.println("Printing Population of Russia: "+Population);
+
+        int mobileNo = 876578756;
+        System.out.println("Printing mobile no: "+mobileNo);
+
+        int invoiceNo =8765546;
+        System.out.println("Print invoice no: "+invoiceNo);
+
+        int year =2023;
+        System.out.println("Print year: "+year);
+
+        double height =5.67;
+        System.out.println("Print height of Vishal: "+height+"inch");
+
+        double weight =64.87;
+        System.out.println("Print weight of Vishal: "+weight+"Kg");
+
+        double Salary =15999.98;
+        System.out.println("Print salary of Vishal: "+Salary+"rupees");
+
+        double Temperature =28.7;
+        System.out.println("Print temperature of faridabad: "+Temperature);
+
+        double Price =212.99;
+        System.out.println("Print price of a cake: "+Price+"rupees");
+
+
+
+
+
+
+
+
+    }
+}
