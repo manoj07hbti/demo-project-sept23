474e7c656ce8c94346cff224292334b86ad01f51
Shree <rohitfzd9897@gmail.com>
2023-10-12 20:45:49 +0530

    revision_variable test

---
+++ src/demo_variables/variable_revision/Test_1.java
@@ -0,0 +1,88 @@
+package demo_variables.variable_revision;
+
+public class Test_1 {
+
+    public static void main(String []args ){
+
+        // 5 String variable
+
+        String countryName;
+
+         countryName= "  INDIAN ";
+
+        System.out.println(" printing  countryName  " + countryName);
+
+        System.out.println( "                        INDIA is located ASIA continent "    );
+
+        String stateName ;
+
+        stateName="   UTTAR PRADESH  ";
+
+        System.out.println( "printing stateName " + stateName  );
+
+
+       String bookName;
+
+       bookName=" The discovery of INDIA  ";
+
+       String writerName;
+       writerName=  " Pt Jawaharlal  Nehru ";
+
+         System.out.println(  " printing bookName   " + bookName  + " writerName  "  + writerName );
+
+       String villageName;
+
+       villageName= " ASHOK NAGAR ";
+
+
+     String brandName ;
+
+     brandName = " LENOVO ";
+
+       System.out.println( "printing brandName  " + brandName ) ;
+
+
+       System.out.println( " 5 int variable ........ ");
+
+
+       int population ;
+
+       population =534676;
+
+       int state;
+
+       state = 28;
+
+       int  student;
+
+       student= 34;
+
+        System.out.println( "printing population "   + population  +"state "  + state  + " student " + student  );
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
