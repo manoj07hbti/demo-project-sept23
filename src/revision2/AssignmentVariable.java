8d4152a5ec22d779cc6595edd0c84dd4f948d05d
shiv8383 <shiv83833@gmail.com>
2023-10-12 19:52:18 +0530

    java class

---
+++ src/revision2/AssignmentVariable.java
@@ -0,0 +1,29 @@
+package revision2;
+
+public class AssignmentVariable {
+
+    public static void main(String[] args) {
+
+        // SYNTAX : Datatype varName;
+
+        String name; // we have a declared variable of String datatype with name as name
+
+        name="Shiv";
+
+        System.out.println(" Printing name "+name);
+
+        int age; // we have declared age variable of int datatype
+
+        age=24;
+
+        System.out.println(" Printing age "+age);
+
+        double salary; // we have declared salary variable of double datatype
+
+        salary=40000.52;
+
+        System.out.println(" Printing salary " +salary);
+    }
+
+
+}
