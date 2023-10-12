3434caa68b23ea2e40f9f61d445c2e4a2e20ba68
shiv8383 <shiv83833@gmail.com>
2023-10-12 18:28:38 +0530

    java class

---
+++ src/revision2/Practice8.java
@@ -0,0 +1,13 @@
+package revision2;
+
+public class Practice8 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" This is my eighth java class....");
+        System.out.println(" So nice of you....");
+        System.out.println(" So sweet of you....");
+        System.out.println(" Very kind of you....");
+        System.out.println(" Thankfull to you....");
+    }
+}
