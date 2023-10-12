5709eab937027fe9d1b8b923749b7f8bf30963e7
shiv8383 <shiv83833@gmail.com>
2023-10-12 18:26:54 +0530

    java class

---
+++ src/revision2/Practice3.java
@@ -0,0 +1,15 @@
+package revision2;
+
+import java.sql.SQLOutput;
+
+public class Practice3 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" This is my third java class....");
+        System.out.println(" You should drive car....");
+        System.out.println(" You should ride bike....");
+        System.out.println(" You should come on time....");
+        System.out.println(" He should ride bike....");
+    }
+}
