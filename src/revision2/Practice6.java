7682111cd87b07bfb4ad50f784dc56c3a2f046a1
shiv8383 <shiv83833@gmail.com>
2023-10-12 18:28:00 +0530

    java class

---
+++ src/revision2/Practice6.java
@@ -0,0 +1,13 @@
+package revision2;
+
+public class Practice6 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" This is my sixth java class");
+        System.out.println(" I watch news on mobile....");
+        System.out.println(" I purchase mobile....");
+        System.out.println(" I purchase bike....");
+        System.out.println(" I purchase car....");
+    }
+}
