79e0bf19bde4cfbaf3be8c0d34471cf2fc12e78a
shiv8383 <shiv83833@gmail.com>
2023-10-12 18:29:14 +0530

    java class

---
+++ src/revision2/practice10.java
@@ -0,0 +1,13 @@
+package revision2;
+
+public class practice10 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" this is my tenth java class");
+        System.out.println(" Apple....");
+        System.out.println(" Samsung....");
+        System.out.println(" Google....");
+        System.out.println(" Oneplus....");
+    }
+}
