0a8680b34e41d9ec27a6132f0b4f482c54ed8588
shiv8383 <shiv83833@gmail.com>
2023-10-12 18:27:41 +0530

    java class

---
+++ src/revision2/Practice5.java
@@ -0,0 +1,14 @@
+package revision2;
+
+public class Practice5 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" This is my fifth java class");
+        System.out.println(" Breakfast....");
+        System.out.println(" Lunch....");
+        System.out.println(" Snaks....");
+        System.out.println(" Dinner....");
+
+    }
+}
