0b25402319c25b8bee726207a47bd8ea48dd325b
Zameer Raza <razazameer905@gmail.com>
2023-10-14 02:24:49 +0530

    Assignment_1

---
+++ src/variables1/Test1.java
@@ -0,0 +1,15 @@
+package variables1;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        String pc="Dell";
+        System.out.println("printing pc:"+pc);
+
+        int quantity=10;
+        System.out.println("printing quantity:"+quantity);
+
+        double price=40000.5;
+        System.out.println("printing price:"+price);
+    }
+}
