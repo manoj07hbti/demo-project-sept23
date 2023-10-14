49fc812438a1d1975c2ab06769343a3f8599f60f
rohanmodii <smrohanmodi@gmail.com>
2023-10-14 18:46:27 +0530

    Assignment 2 12 Oct.

---
+++ src/AssignmentVariables/AssignmentSquareandCube.java
@@ -0,0 +1,22 @@
+package AssignmentVariables;
+
+public class AssignmentSquareandCube
+{
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=5;
+        int square;
+        int cube;
+
+        square=a*a;
+        System.out.println("The square of Given integor 'a' is :" +square);
+
+        cube=b*b*b;
+        System.out.println("The cube of Given Integor 'b' is :" +cube);
+
+        System.out.println("End");
+
+
+    }
+}
