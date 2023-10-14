a7488318068aba3816f732a8d695159f902cfb15
Mayank6918-svh <mayanksingh25104@gmail.com>
2023-10-14 12:15:15 +0530

    5 java classes + variables(string+int+double)

---
+++ src/Variables/DemoAdvanceVariables.java
@@ -0,0 +1,39 @@
+package Variables;
+
+public class DemoAdvanceVariables {
+    public static void main(String[] args) {
+
+        int x = 100;
+        int y = 3;
+        int add = x + y;
+
+        System.out.println("Addition of x and y..." + add);
+
+        int sub = x - y;
+        System.out.println("Subtraction of x and y..." + sub);
+
+        int multi = x * y;
+
+        System.out.println("Multiplication of x and y..." + multi);
+
+        double divide = (double) x / y;
+
+        System.out.println("Dividation of x and y..." + divide);
+
+        int a = 2;
+        int squre = a * a;
+
+        System.out.println("Square of a..." + squre);
+
+        int b = 5;
+        int cube = b * b;
+
+        System.out.println("Square of b..." + cube);
+
+        int p = 50;
+        double percentage = (double) p * 10 / 100;
+
+        System.out.println("Percantage of 100..." + percentage);
+
+    }
+}
