2e4b7ddcc9ba991ca95c777dd380ea7709863464
Vishal Kumar <vk9555411@gmail.com>
2023-10-14 20:28:37 +0530

    Revision and Advance

---
+++ src/revision/AssignmentAdvanceVariable.java
@@ -0,0 +1,42 @@
+package revision;
+
+public class AssignmentAdvanceVariable {
+
+    public static void main(String[] args) {
+
+        //write a code for making square of number
+
+        int a = 8;
+        a = a * a;
+        System.out.println("Print square of two: " + a);
+
+        //write a code for making cube of number
+        int b = 3;
+        b = b * b * b;
+        System.out.println("Print cube of Three: " + b);
+
+        //write a code to calculate %    number * pecentage/100
+        //  200*10/100 == 20
+
+        double Percentage = 2375;
+        Percentage = (double) 257 * 64 / 100;
+        System.out.println("Printing percentage of Vishal: " + Percentage);
+
+        int x = 20;
+        int y = 3;
+
+        int result = x + y;
+        System.out.println("Printing adding x and y: " + result);
+
+        result = x - y;
+        System.out.println("Printing subtraction of x and y: " + result);
+
+        result = x * y;
+        System.out.println("Printing multiply of x and y: " + result);
+
+        double divide = (double) x / y;
+        System.out.println("Printing divide of x and y: " + divide);
+
+
+    }
+}
