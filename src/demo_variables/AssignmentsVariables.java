2a8fd3d69a0e386f6203303d553733edd9cab066
dkang <Dkangryboy7@gmail.com>
2023-10-14 17:48:37 +0530

    Assignment variables demo

---
+++ src/demo_variables/AssignmentsVariables.java
@@ -0,0 +1,47 @@
+package demo_variables;
+
+public class AssignmentsVariables {
+    public static void main(String[] args) {
+
+        String Countryname;
+
+        Countryname="india";
+
+        System.out.println("country name is "+Countryname);
+
+        int population;
+
+        population=13000000;
+
+        System.out.println("my country population "+population);
+
+        double income;
+
+        income=112000.50;
+
+        System.out.println("income is "+income);
+
+        String House="Ghar";
+
+        int member=13;
+
+        double percentage=50.5;
+
+        System.out.println("house:"+ House+" and member:"+member+" percentage:" +percentage);
+
+        int x=50;
+        int y=4;
+
+        int result=x+y;
+        System.out.println("addition of x and y:"+result);
+        result=x-y;
+        System.out.println("is:"+result);
+        result=x*y;
+        System.out.println("multi is:"+result);
+        double divide=(double)x/y;
+        System.out.println("division is:"+divide);
+
+
+    }
+
+    }
