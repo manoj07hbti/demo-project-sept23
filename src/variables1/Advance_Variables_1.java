4a403908c4acf366b5e64ef6aa14e2ad34801933
Zameer Raza <razazameer905@gmail.com>
2023-10-14 17:04:53 +0530

    advance_Variable_Assignment

---
+++ src/variables1/Advance_Variables_1.java
@@ -0,0 +1,13 @@
+package variables1;
+
+public class Advance_Variables_1 {
+
+    public static void main(String[] args) {
+
+        String CityName="pune";
+        int population=4320000;
+        double perPersonIncome=44423.43;
+        System.out.println("printing CityName :"+CityName +" population :"+population +" income :"+perPersonIncome);
+
+    }
+}
