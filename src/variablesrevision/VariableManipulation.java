0201e73026f65540c92848f29b6a846102815473
dkang <Dkangryboy7@gmail.com>
2023-10-15 16:33:43 +0530

    variable manipulation practice

--- src/variablesrevision/VariableManipulation.java
+++ src/variablesrevision/VariableManipulation.java
@@ -18,5 +18,40 @@ public class VariableManipulation {
         double divide=(double)x/y;
         System.out.println("division of x and y is: " +divide);
 
+        int result1=9;
+
+        result1++;
+        System.out.println("increament number is: "+result1);
+
+        result1--;
+        System.out.println("decreament number is: "+result1);
+
+        int number=11;
+
+        number++;
+        System.out.println("increament number is: "+number);
+
+        number--;
+        System.out.println("decreament number is: "+number);
+
+        number--;
+        System.out.println("decreament number is: "+number);
+
+        x=9;
+        y=5;
+
+        x+=y;
+        System.out.println("addition of x and y is :"+x);
+        x-=y;
+        System.out.println("subtraction of x and y is:"+x);
+        x*=y;
+        System.out.println("multiply of x and y is: "+x);
+
+        x/=y;
+
+        System.out.println("division of x and y is: "+x);
+
+
+
         }
 }
