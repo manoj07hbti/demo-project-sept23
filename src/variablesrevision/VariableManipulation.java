b6f3fd2b97131397a54921aef617f9fd03976437
dkang <Dkangryboy7@gmail.com>
2023-10-15 16:03:07 +0530

    variable manipulation practice

---
+++ src/variablesrevision/VariableManipulation.java
@@ -0,0 +1,22 @@
+package variablesrevision;
+
+public class VariableManipulation {
+    public static void main(String[] args) {
+
+        int x = 40;
+        int y = 6;
+
+        int result = x + y;
+        System.out.println("addition of x and y is: " + result);
+
+        result = x - y;
+        System.out.println("subtraction of x and y is: " + result);
+
+        result = x * y;
+        System.out.println("multiply of x and y is: " + result);
+
+        double divide=(double)x/y;
+        System.out.println("division of x and y is: " +divide);
+
+        }
+}
