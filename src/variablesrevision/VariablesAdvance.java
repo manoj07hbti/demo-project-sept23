4df73d7a4006caf4bc9d4c45b7bc2adc85c31a06
dkang <Dkangryboy7@gmail.com>
2023-10-15 15:41:17 +0530

    variable advance practice

---
+++ src/variablesrevision/VariablesAdvance.java
@@ -0,0 +1,34 @@
+package variablesrevision;
+
+public class VariablesAdvance {
+
+    public static void main(String[] args) {
+
+        String Maneger="Gagan";
+        System.out.println("Maneger is:"+Maneger);
+
+        String Teamleader="Deepak";
+        System.out.println("Teamleader is: "+Teamleader);
+
+        String Relationshipofficer="Askok";
+        System.out.println("R.O Name is :"+Relationshipofficer);
+
+        int Manegersalary=100000;
+        System.out.println("Maneger salary is: "+Manegersalary);
+
+        int TLsalary=50000;
+        System.out.println("T.L salary is :"+TLsalary);
+
+        int Rosalary=20000;
+        System.out.println("R.O salary is :"+Rosalary);
+
+        double Mratio=56.3;
+        System.out.println("Maneger performance ratio is: "+Mratio);
+
+        double Tlratio=80.88;
+        System.out.println("T.L performance ratio is: "+Tlratio);
+
+        double Roratio=99.9;
+        System.out.println("R.O performance ratio is: "+Roratio);
+    }
+}
