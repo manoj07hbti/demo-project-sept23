7a4cfbf816022d1f5e4cf960cfd6dff22f12528d
Shree <rohitfzd9897@gmail.com>
2023-10-16 13:10:37 +0530

    revision_variable test

---
+++ src/manipulation/revision_manipulation/Test_1.java
@@ -0,0 +1,68 @@
+package manipulation.revision_manipulation;
+
+public class Test_1 {
+
+    public static void main( String [] args){
+
+        int a =20;
+
+        int b= 3;
+
+        int result  = a+b;
+
+        System.out.println(" Addition of a and b is : " + result );
+
+
+        result= a-b ;
+
+        System.out.println(" subtraction of a and b is : " + result);
+
+
+
+        result = a*b;
+
+        System.out.println(" multi  of a and b is : " + result );
+
+
+          double divide =(double) a/b;
+
+        System.out.println("divide of a and b is :" + result );
+
+       // increment decrement  operator
+
+        int number =3;
+
+        number++;
+        System.out.println("number after increment  operator: " + result );
+
+        number-- ;
+
+        System.out.println( " number after decrement operator "  +result );
+
+
+         a= 8;
+
+         b =4;
+
+         a+=b;   //a=a+b;
+
+
+        System.out.println(" printing a " +a);
+
+        a-=b;  //a= a-b;
+
+        System.out.println(" Printing after -a "  +a);
+
+
+        a/=b;  //a = a/b ;
+
+        System.out.println(" After divide a ;"+a );
+
+
+
+
+
+    }
+
+
+}
