de5bf0aedd234a9344dd3f197b1319d0c51af90e
Shree <rohitfzd9897@gmail.com>
2023-10-16 14:21:26 +0530

    manipulation square

---
+++ src/manipulation/revision_manipulation/Assignent_of_square.java
@@ -0,0 +1,76 @@
+package manipulation.revision_manipulation;
+
+public class Assignent_of_square {
+
+    public static void main(String[] args) {
+
+
+        //making square numbers
+
+
+        int a = 4;
+
+        int square = a * a;
+
+        System.out.println("square of 4 is : " + a * a);
+
+        a = 5;
+        System.out.println("square of 5 is : " + a * a);
+
+
+        a = 6;
+        System.out.println("square of 6 is : " + a * a);
+
+
+        a = 7;
+        System.out.println("square of 7 is : " + a * a);
+
+        a = 8;
+        System.out.println("square of 8 is : " + a * a);
+
+        a = 9;
+        System.out.println("square of 9 is : " + a * a);
+
+
+        a = 10;
+        System.out.println("square of 10 is : " + a * a);
+
+
+        a = 11;
+        System.out.println("square of 11 is : " + a * a);
+
+        a = 12;
+        System.out.println("square of 12 is : " + a * a);
+
+        a = 13;
+        System.out.println("square of 13 is : " + a * a);
+        a = 14;
+        System.out.println("square of 14 is : " + a * a);
+        a = 15;
+        System.out.println("square of 15 is : " + a * a);
+        a = 16;
+        System.out.println("square of 16 is : " + a * a);
+        a = 17;
+        System.out.println("square of 17 is : " + a * a);
+        a = 18;
+        System.out.println("square of 18 is : " + a * a);
+        a = 19;
+        System.out.println("square of 19 is : " + a * a);
+        a = 20;
+        System.out.println("square of 20 is : " + a * a);
+        a = 21;
+        System.out.println("square of 21 is : " + a * a);
+        a = 22;
+        System.out.println("square of 22 is : " + a * a);
+        a = 23;
+        System.out.println("square of 23 is : " + a * a);
+        a = 24;
+        System.out.println("square of 24 is : " + a * a);
+        a = 25;
+        System.out.println("square of 25 is : " + a * a);
+
+
+    }
+
+
+}
