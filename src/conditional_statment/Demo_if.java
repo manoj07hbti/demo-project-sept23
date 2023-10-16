fd38c5eb29f69d82e463ec1be17370b4109f0432
Shree <rohitfzd9897@gmail.com>
2023-10-16 23:14:10 +0530

    manipulation square

---
+++ src/conditional_statment/Demo_if.java
@@ -0,0 +1,115 @@
+package conditional_statment;
+
+public class Demo_if {
+
+
+    public static void main(String [] args ){
+
+        /*
+          if(expression/condition){
+
+             //code
+       }*/
+
+   int age =20;
+
+   if( age >18){
+
+       System.out.println( "condition is eligiable for D.l");
+
+       double price =5646.45;
+
+       if( price >5000   ){
+
+           System.out.println("product is costly......");
+
+           String city= "Pune";
+
+           if(city=="Pune"){
+
+               System.out.println("candidate belongs to Pune ......");
+           }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       }
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/manipulation/revision_manipulation/Assignment_cube.java
@@ -0,0 +1,14 @@
+package manipulation.revision_manipulation;
+
+public class Assignment_cube {
+
+
+    int a = 2 ;
+
+    int square = a * a  ;
+
+
+
+
+
+    }
