f86311b76dde8aeb13bd62ec6534c3bb713ddcbe
Zameer Raza <razazameer905@gmail.com>
2023-10-17 12:38:19 +0530

    Code for making square of a Number

---
+++ src/demo_variables/Square_Of_Number.java
@@ -0,0 +1,20 @@
+package demo_variables;
+
+public class Square_Of_Number {
+
+    public static void main(String[] args) {
+
+        int num=20;
+        int percent=5;
+
+
+
+        double result=num*percent/100;
+
+        System.out.println("percentage is " +result +"%");
+    }
+
+
+
+
+}
