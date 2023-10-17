0158b15358a0ca72944f75d9d0d4e9386c5af261
Zameer Raza <razazameer905@gmail.com>
2023-10-17 20:30:53 +0530

    Condition if Demo1

---
+++ src/conditional_statement/Test1.java
@@ -0,0 +1,20 @@
+package conditional_statement;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+     /*if(expression/condition){
+        //code}
+     */
+        int age = 19;
+
+        if (age > 18) {
+            System.out.println("candidate is eligible for DL. ");
+        }
+
+    }
+        }
+
+
+
