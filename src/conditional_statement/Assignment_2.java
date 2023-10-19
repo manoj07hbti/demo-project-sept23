f42dc9c23c1dcd669efe36d54188730b4a8801f4
Zameer Raza <razazameer905@gmail.com>
2023-10-19 20:15:38 +0530

    percentage calculation

---
+++ src/conditional_statement/Assignment_2.java
@@ -0,0 +1,17 @@
+package conditional_statement;
+
+public class Assignment_2 {
+    public static void main(String[] args) {
+        //code for finding even and odd number
+        int num = 5;
+        if (num > 0) {
+            if (num%2==0) {
+                System.out.println("number is even ");
+            } else {
+                System.out.println("number is odd ");
+            }
+
+        }
+
+    }
+}
