90f31d7db48aa4b8ef17b86cad052cb6a0b769d3
Zameer Raza <razazameer905@gmail.com>
2023-10-21 15:43:45 +0530

    And operator with all 4 condition

---
+++ src/operators/And_Operator.java
@@ -0,0 +1,44 @@
+package operators;
+
+public class And_Operator {
+    // And operator symbol- (&&)
+    public static void main(String[] args) {
+        /* code for to find a laptop witch price is not above is 30000 and brand is HP
+
+         */
+// 1- True && True= true
+        double price = 25000.34;
+        String brand = "HP";
+        if (price <= 30000 && brand == "HP") {
+            System.out.println("laptop's quality is good ");
+        } else {
+            System.out.println("laptop is not a good quality");
+        }
+
+// 2- True && False= False
+        price = 40000;
+        brand = " lenovo";
+        if (price >= 35000 && brand == "Dell") {
+            System.out.println("laptop is good quality ");
+        } else {
+            System.out.println("laptop is not good quality");
+        }
+// 3- False && True= False
+        price = 20000;
+        brand = "HCl";
+        if (price >= 25000 && brand == "HCl") {
+            System.out.println("laptop is good quality");
+        } else {
+            System.out.println("laptop is not good quality");
+        }
+// 4- False && False= False
+        price = 50000;
+        brand = "Apple";
+        if (price >= 60000 && brand == "dell") {
+            System.out.println("laptop is good quality");
+        } else {
+            System.out.println("laptop is not good quality");
+
+        }
+    }
+}
