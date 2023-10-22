afb2b0031fcd8da7949d85ae50f688faf057ae4d
shiv8383 <shiv83833@gmail.com>
2023-10-22 21:37:41 +0530

    TestQ4

---
+++ src/test_22_oct_2023/test4.java
@@ -0,0 +1,22 @@
+package test_22_oct_2023;
+
+public class test4 {
+
+    public static void main(String[] args) {
+
+        // Q5;
+
+        String nationality="Indian";
+        String state="Pune";
+
+        if(nationality=="Indian" && state=="Maharastra"){
+
+            System.out.println(" Person Nationality is Indian ");
+
+        }else{
+
+            System.out.println( "Person Nationality is not Indian ");
+        }
+
+    }
+}
