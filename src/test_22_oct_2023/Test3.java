5c5e68c76aa9bec07d5fe9af0c149770d52e2040
shiv8383 <shiv83833@gmail.com>
2023-10-22 19:27:25 +0530

    testQ3

---
+++ src/test_22_oct_2023/Test3.java
@@ -0,0 +1,20 @@
+package test_22_oct_2023;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+
+        // write a code to check if any given number is  even or odd number;
+
+        int number=17;
+
+        if(number==18){
+
+            System.out.println(" Even number ");
+
+        }else{
+
+            System.out.println( " Odd number ");
+        }
+    }
+}
