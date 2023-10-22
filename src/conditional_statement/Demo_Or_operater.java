c9762b9a9815bf4887b8f86f4ba1269e4bd15556
dkang <Dkangryboy7@gmail.com>
2023-10-22 16:56:47 +0530

    trying to do with or operater

---
+++ src/conditional_statement/Demo_Or_operater.java
@@ -0,0 +1,12 @@
+package conditional_statement;
+
+public class Demo_Or_operater {
+    public static void main(String[] args) {
+        int marks=35;
+        if(marks>=33||marks<60||marks>=60){
+            System.out.println("student get second division");
+        }else{
+            System.out.println("student not get second division");
+        }
+    }
+}
