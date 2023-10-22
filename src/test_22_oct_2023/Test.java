ca7a3d43ce1c2295bd992d70844c28a8e642b942
shiv8383 <shiv83833@gmail.com>
2023-10-22 19:26:47 +0530

    test1

---
+++ src/test_22_oct_2023/Test.java
@@ -0,0 +1,22 @@
+package test_22_oct_2023;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        // write a code to check eligible voter in Pune;
+
+        int number= 17;
+
+        if(number==18){
+
+            System.out.println(" Eligible voter pune ");
+
+
+        }else{
+
+            System.out.println(" Not eligible voter pune ");
+        }
+
+    }
+}
