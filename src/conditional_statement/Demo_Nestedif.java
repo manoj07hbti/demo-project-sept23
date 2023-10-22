505f57605fb465b8f692cb5468d056e973dfeffb
dkang <Dkangryboy7@gmail.com>
2023-10-22 13:16:23 +0530

    revise practice Demo if and else practice

---
+++ src/conditional_statement/Demo_Nestedif.java
@@ -0,0 +1,28 @@
+package conditional_statement;
+
+public class Demo_Nestedif {
+    public static void main(String[] args) {
+        int num=3;
+        if(num>0){
+
+            if(num%2==0){
+                System.out.println("this is even number");
+            }else{
+                System.out.println("this number is odd number");
+            }
+        }else{
+            System.out.println("please enter the number is grater then 0");
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+}
+
