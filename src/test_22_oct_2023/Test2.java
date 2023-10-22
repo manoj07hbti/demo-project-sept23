648d75e6aed3ad699657c73266680ed925947142
shiv8383 <shiv83833@gmail.com>
2023-10-22 20:43:45 +0530

    testQ2

---
+++ src/test_22_oct_2023/Test2.java
@@ -0,0 +1,17 @@
+package test_22_oct_2023;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        // Write a code to find percentage of given number example: 5% of 500;
+
+        int number=500;
+        int percentage=5;
+
+        double result=(double)number*percentage/100;
+
+        System.out.println(" find percentage of given number " +result);
+
+    }
+}
