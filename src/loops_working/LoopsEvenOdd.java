1bc82128b9f2122b7f23ba287760b59bf9043f46
shiv8383 <shiv83833@gmail.com>
2023-10-31 19:52:18 +0530

    Loops1to100

---
+++ src/loops_working/LoopsEvenOdd.java
@@ -0,0 +1,23 @@
+package loops_working;
+
+public class LoopsEvenOdd {
+
+    public static void main(String[] args) {
+
+        // Print all even and odd number between 1 to 100 using for;
+
+        for(int i=1; i<=200; i++){
+
+            if(i%2==0){
+
+                System.out.println(i + " Even Number ");
+
+            }else{
+
+                System.out.println(i + " Odd Nunber ");
+            }
+
+
+        }
+    }
+}
