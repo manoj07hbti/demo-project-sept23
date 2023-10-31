0258518c0baf01f7097d22b5c1b63fcfe0866d5c
Shree <rohitfzd9897@gmail.com>
2023-10-31 18:51:14 +0530

    assignment

--- src/loops_working/Assignment_For_Loop.java
+++ src/loops_working/Assignment_For_Loop.java
@@ -1,15 +1,15 @@
 package loops_working;
 
-public class EvenOdd_number_by_LOOP {
+public class Assignment_For_Loop {
     public static void main(String[] args) {
 
         //   Print all even and odd number between 1 to 100 using for loop
 
         for (int i = 1; i <= 100; i++) {
             if (i % 2 == 0) {
-                System.out.println("This is  even number ..." + i);
+                System.out.println("This is  even number ......." + i);
             } else {
-                System.out.println(" This is odd number " + i);
+                System.out.println(" This is odd number "  + i);
             }
 
         }
---
+++ src/loops_working/assignments/Square_using_for_Loop.java
@@ -0,0 +1,14 @@
+package loops_working.assignments;
+
+public class Square_using_for_Loop {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("square of " + i + " = " + i * i);
+        }
+
+
+    }
+
+}
