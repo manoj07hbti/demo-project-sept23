28e8283f781d0c312b4111eb0e47b58dbd8e0a34
shiv8383 <shiv83833@gmail.com>
2023-10-31 19:43:06 +0530

    LoopsTable5And10

---
+++ src/loops_working/Loops.java
@@ -0,0 +1,25 @@
+package loops_working;
+
+public class Loops {
+
+    public static void main(String[] args) {
+
+        // Print table of 5 and 10 using for loop;
+
+        int number=5;
+
+        for(int i=1; i<=10; i++){
+
+            System.out.println(number + " X " +i+ " = " +number*i);
+
+        }
+        // table 10;
+
+        number=10;
+
+        for(int i=1; i<=10; i++){
+
+            System.out.println(number + " X " +i+ " = " +number*i);
+        }
+    }
+}
