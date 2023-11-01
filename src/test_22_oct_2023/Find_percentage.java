ce4be40ba835a7877cbc44b97c61068490b039a8
dkang <Dkangryboy7@gmail.com>
2023-11-02 00:18:32 +0530

    switch statement task complete

--- src/test_22_oct_2023/Find_percentage.java
+++ src/test_22_oct_2023/Find_percentage.java
@@ -2,9 +2,9 @@ package test_22_oct_2023;
 
 public class Find_percentage {
     public static void main(String[] args) {
-        int a = 500;
+        int a = 255;
         int b = 5;
-        int result = a * b / 100;
+       double  result = (double)a * b / 100;
         System.out.println("percentage of given number is: " + result);
     }
 }
