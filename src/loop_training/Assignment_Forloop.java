f83c6b6234f7b900d7af97c25b8bab720b85a748
dkang <Dkangryboy7@gmail.com>
2023-11-03 23:14:55 +0530

    for loop assignment complete

---
+++ src/loop_training/Assignment_Forloop.java
@@ -0,0 +1,25 @@
+package loop_training;
+
+public class Assignment_Forloop {
+    public static void main(String[] args) {
+        /*Print table of 5 and 10 using for loop */
+
+        int number = 5;
+
+        for (int a = 1; a <= 10; a++) {
+
+            System.out.println(number + " x " + a + " = " + number * a);
+        }
+        /*Print table of 5 and 10 using for loop */
+
+
+        int num = 10;
+
+        for (int b = 1; b <= 10; b++) {
+
+            System.out.println(num + " x " + b + " = " + num * b);
+        }
+    }
+
+
+}
