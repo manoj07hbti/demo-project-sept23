7bffc05a35cbef8e2f3522ab1a6f1f432e537f42
dkang <Dkangryboy7@gmail.com>
2023-11-03 22:57:09 +0530

    for loop practice

---
+++ src/loop_training/For_Loop.java
@@ -0,0 +1,12 @@
+package loop_training;
+
+public class For_Loop {
+
+    public static void main(String[] args) {
+
+        for(int a=1; a<=10; a++){
+
+            System.out.println("this for training purpose " + a);
+        }
+    }
+}
