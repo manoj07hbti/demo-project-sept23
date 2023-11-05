4ad617e432362ccb36855b919706221577b22a5d
dkang <Dkangryboy7@gmail.com>
2023-11-05 12:58:03 +0530

    for loop assignment complete

--- src/loop_training/Odd_Even_by_Forloop.java
+++ src/loop_training/Odd_Even_by_Forloop.java
@@ -5,10 +5,15 @@ public class Odd_Even_by_Forloop {
     public static void main(String[] args) {
 
 
-        for(int a=99;  a%2==0 ; a++){
-            System.out.println("this is even number");
+        for (int a = 1; a <= 100; a++) {
+
+            if (a % 2 == 0) {
+                System.out.println("this is even number " + a);
+            } else {
+                System.out.println("this is odd number  " + a);
+            }
 
         }
-        System.out.println("this is odd number");
+
     }
 }
