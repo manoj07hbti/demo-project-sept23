c5d54e3015e706fc70204b2ed756edfc6576dd23
dkang <Dkangryboy7@gmail.com>
2023-11-11 16:09:53 +0530

    assignment continue with array by advance for loop is complete

---
+++ src/loop_training/Demo_Continue.java
@@ -0,0 +1,40 @@
+package loop_training;
+
+public class Demo_Continue  {
+
+    public static void main(String[] args) {
+        int number=10;
+
+        for(int a=0; a<=10; a++){
+
+
+            if(a==7){
+                continue;
+            }
+
+            System.out.println("printing number: "+a);
+
+
+        }
+        //print marks of Students in a class and if you find any students with less than 33 marks
+        //   then you need break.
+        //
+        //   continue
+        int marks[]={58,56,88,87,33,46};
+
+        for( int i=0;i<marks.length;i++){
+
+            if(i==4){
+                continue;
+            }
+
+            System.out.println("printing marks: "+marks[i]);
+        }
+
+
+
+
+
+
+    }
+}
