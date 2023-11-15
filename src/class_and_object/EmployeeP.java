acfdd948eeb91951ff5287c58c932749f2e014dd
sachin31java <Sachin99kumar59@gmail.com>
2023-11-15 15:55:12 +0530

    third commit of class and object

--- src/class_and_object/EmployeeP.java
+++ src/class_and_object/EmployeeP.java
@@ -20,5 +20,7 @@ public class EmployeeP {
         System.out.println("Name: "+Employee2.Name+" age: "+Employee2.age+" dept: "+Employee2.Dept+" salary: "+Employee2.salary);
         EmployeeP Employee3=new EmployeeP("Nikhil",  22,"Account",52000);
         System.out.println("Name :"+Employee3.Name+" age: "+Employee.age+" dept: "+Employee3.Dept+" salary: "+Employee3.salary);
+        EmployeeP Employee4=new EmployeeP("Rk",21,"ABC",9804.10);
+        System.out.println("Name: "+Employee4.Name+" age: "+Employee4.age+" dept: "+Employee4.Dept+" salary: "+Employee4.salary);
     }
 }
