4ad77e5f4d0253dfa61df44e0fa1ca777b48432c
dkang <Dkangryboy7@gmail.com>
2023-11-26 17:14:37 +0530

    assignment complete calculater app

---
+++ src/methods/Calculater_App.java
@@ -0,0 +1,41 @@
+package methods;
+
+public class Calculater_App {
+
+    public void addition() {
+        int a = 12;
+        int b = 14;
+        int result = a + b;
+        System.out.println("Addition number is : " + result);
+    }
+
+    public void subtraction() {
+        int a = 2;
+        int b = 8;
+        int result = a - b;
+        System.out.println("subtraction number is: " + result);
+
+    }
+
+    public void multiplication() {
+        int a = 8;
+        int b = 9;
+        int result = a * b;
+        System.out.println("multiplication number is: " + result);
+    }
+
+    public void division() {
+        int a = 85;
+        int b = 5;
+        double result = (double) a / b;
+        System.out.println("division number is: " + result);
+    }
+
+    public static void main(String[] args) {
+        Calculater_App obj = new Calculater_App();
+        obj.addition();
+        obj.subtraction();
+        obj.multiplication();
+        obj.division();
+    }
+}
