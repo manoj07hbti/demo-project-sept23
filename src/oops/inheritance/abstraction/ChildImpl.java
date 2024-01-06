47724e86f4a78faf22c16e20840929924dd90b11
Manoj Kumar <manojkumar.pcf@gmail.com>
2024-01-06 21:03:52 +0530

    OOPS  : Day3

---
+++ src/oops/inheritance/abstraction/ChildImpl.java
@@ -0,0 +1,18 @@
+package oops.inheritance.abstraction;
+
+public class ChildImpl implements DemoInterface{
+    @Override
+    public void m1() {
+
+    }
+
+    @Override
+    public void m2() {
+
+    }
+
+    @Override
+    public void m3() {
+
+    }
+}
---
+++ src/oops/inheritance/abstraction/DemoInterface.java
@@ -0,0 +1,9 @@
+package oops.inheritance.abstraction;
+
+public interface DemoInterface {
+    // we do not need any abstract keyword to represent abstract method , 100 % abstraction
+    public void m1(); // this is abstract method
+    public void m2();// this is abstract method
+    public void m3();// this is abstract method
+
+}
