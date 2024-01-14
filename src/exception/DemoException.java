7873a09fe9b04dbe4fd11da699b0554ac56d296b
Manoj Kumar <manojkumar.pcf@gmail.com>
2024-01-14 21:16:41 +0530

    Exception Day2

--- src/exception/DemoException.java
+++ src/exception/DemoException.java
@@ -6,15 +6,27 @@ public class DemoException {
 
         System.out.println("Before Division ");
         int result=0;
+
+        //TODO Q1: CAN WE HAVE TRY BLOCK WITHOUT CATCH BLOCK ?
+        //TODO ANS: NO, EVERY TRY BLOCK SHOULD HAVE AT LEAST ONE CATCH OR FINALLY BLOCK
+        //TODO Q2: CAN WE HAVE MULTIPLE CATCH BLOCK IN SINGLE TRY BLOCK ?
+        //TODO YES , WE NEED TO MANTAIN HIRERACHY OF EXCEPTION(CHILD CLASS EXCEPTION WILL BE COUGHT FIRST AND THEN PARENT)
         try {
              result = a / b;
         }
-        catch (Exception e) {
-            System.out.println("Exception occurred :" + e);
+        catch (ArithmeticException e) {
+            System.out.println("ArithmeticException occurred :" + e);
+        }
+        catch (RuntimeException e) {
+            System.out.println("RuntimeException occurred :" + e);
         }
 
+        finally{
+            System.out.println("This is finally BLOCK ");
 
+        }
         System.out.println("Division is: "+result);
+
     }
 
     public void printArray(){
@@ -33,7 +45,7 @@ public class DemoException {
 
         DemoException obj= new DemoException();
         obj.divide(4,0);
-         obj.printArray();
+        //obj.printArray();
 
     }
 }
